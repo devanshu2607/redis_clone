e2bea0193cfabdba9e9ec00c27aecb6371a75a0d
devanshu <devanshukothari26@gmail.com>
2026-07-03 00:02:05 +0530

    debugging parser removed

--- src/test/java/com/cacheserver/ConC/ConCApplicationTests.java
+++ src/test/java/com/cacheserver/ConC/ConCApplicationTests.java
@@ -1,13 +1,26 @@
 package com.cacheserver.ConC;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+import com.cacheserver.ConC.command.Command;
+import com.cacheserver.ConC.command.CommandType;
+import com.cacheserver.ConC.parser.PlainTextParser;
+import com.cacheserver.ConC.router.RequestRouter;
+
 class ConCApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void testParserAndRouter() {
+        PlainTextParser parser = new PlainTextParser();
+        Command command = parser.parse("SET user1 Rahul EX 30");
 
+        assertEquals(CommandType.SET, command.getType());
+        assertEquals("user1", command.getKey());
+        assertEquals(3, command.getArgs().length - 1);
+
+        RequestRouter router = new RequestRouter(3);
+        int workerIndex = router.getWorkerIndex(command);
+        assertTrue(workerIndex >= 0 && workerIndex < 3);
+    }
 }
